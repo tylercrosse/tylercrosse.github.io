b86a196128a5aaf67fb5e054c22ddb073fda9f21
Tyler Crosse <tyler.crosse@axiomlaw.com>
2020-06-18 19:37:04 -0700

    Subtle change to personal logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -82,7 +82,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/tc-512.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon-512.png`, // This path is relative to the root of the site.
       },
     },
     {
